e1ec93e2d16ef34642220b874837b91fa3da74d8
Mikhail Trishchenkov <kriomant@gmail.com>
2012-11-26 11:25:42 +0700

    Render tab icons (selected and unselected) for platform v5.
    Add support for target file suffixes in image variants.

--- build.sbt
+++ build.sbt
@@ -8,6 +8,7 @@ libraryDependencies ++= Seq(
 	"org.apache.xmlgraphics" % "batik-transcoder" % "1.7",
 	"org.apache.xmlgraphics" % "batik-codec" % "1.7",
 	"ch.qos.logback" % "logback-classic" % "0.9.30",
+	"com.jhlabs" % "filters" % "2.0.235",
 	"org.scalatest" % "scalatest_2.10.0-RC2" % "1.8" % "test"
 )
 
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,10 +2,9 @@ package net.kriomant.android_svg_res
 
 import java.io.{File, FileOutputStream}
 import org.slf4j.LoggerFactory
-import java.awt.image.{BufferedImage, ByteLookupTable, LookupOp}
-import org.w3c.dom.svg.SVGDocument
+import java.awt.image.{BufferedImage}
 import org.apache.batik.dom.svg.SVGOMDocument
-import java.awt.{AlphaComposite, Color, GradientPaint, Rectangle}
+import java.awt.{AlphaComposite, Color, GradientPaint}
 
 object Main {
 	val logger = LoggerFactory.getLogger(getClass)
@@ -23,7 +22,12 @@ object Main {
 	val DENSITIES = Seq("ldpi", "mdpi", "hdpi", "xhdpi")
 
 	type Renderer = (SVGOMDocument, Int, Int) => BufferedImage
-	case class ImageVariant(renderer: Renderer, qualifiers: ResourceQualifiers, sizes: Seq[(Int, Int)])
+	case class ImageVariant(
+		renderer: Renderer,
+		qualifiers: ResourceQualifiers,
+		sizes: Seq[(Int, Int)],
+		nameSuffix: Option[String] = None
+	)
 
 	/** Get image sizes for given image kind for ldpi, mdpi, hdpi, xhdpi densities.
 	  */
@@ -64,9 +68,19 @@ object Main {
 			svg.render, ResourceQualifiers.empty, Seq((24,24), (32,32), (48,48), (64,64))
 		))
 
-		case ImageKind.Tab => Seq(ImageVariant(
-			svg.render, ResourceQualifiers.empty, Seq((24,24), (32,32), (48,48), (64,64))
-		))
+		case ImageKind.Tab => Seq(
+			ImageVariant(
+				renderTabV5Unselected, ResourceQualifiers(platformVersion=Some(5)), Seq((24,24), (32,32), (48,48), (64,64)),
+				nameSuffix=Some("unselected")
+			),
+			ImageVariant(
+				renderTabV5Selected, ResourceQualifiers(platformVersion=Some(5)), Seq((24,24), (32,32), (48,48), (64,64)),
+				nameSuffix=Some("selected")
+			),
+			ImageVariant(
+				svg.render, ResourceQualifiers.empty, Seq((24,24), (32,32), (48,48), (64,64))
+			)
+		)
 	}
 
 	def main(args: Array[String]) {
@@ -98,7 +112,8 @@ object Main {
 					drawableDirectory.mkdir()
 				}
 
-				val targetFile = new File(drawableDirectory, "%s.png" format baseName)
+				val suffix = variant.nameSuffix.map("_" + _).getOrElse("")
+				val targetFile = new File(drawableDirectory, "%s%s.png" format (baseName, suffix))
 
 				// Load SVG document.
 				val svgDocument = svg.load(sourceFile)
@@ -122,6 +137,58 @@ object Main {
 				|     of images. If they are required, but you don't need ones, use empty string.
 			""".stripMargin)
 		}
+	}
+
+	def renderTabV5Unselected(doc: SVGOMDocument, width: Int, height: Int): BufferedImage = {
+		val paddings = Map(24 -> 1, 32 -> 2, 48 -> 3, 64 -> 4)
+		val padding = paddings(width)
+		val iconSize = width - 2*padding
+
+		val asset = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB)
+		val gc = asset.createGraphics()
+		gc.setPaint(new Color(0x80,0x80,0x80))
+		gc.fillRect(padding, padding, iconSize, iconSize)
+
+		val icon = svg.render(doc, iconSize, iconSize)
+		gc.setComposite(AlphaComposite.DstIn)
+		gc.drawImage(icon, null, padding, padding)
+		gc.dispose()
+
+		asset
+	}
+
+	def renderTabV5Selected(doc: SVGOMDocument, width: Int, height: Int): BufferedImage = {
+		val paddings = Map(24 -> 1, 32 -> 2, 48 -> 3, 64 -> 4)
+		val padding = paddings(width)
+		val iconSize = width - 2*padding
+
+		val icon = svg.render(doc, iconSize, iconSize)
+
+		val preGlow = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB)
+		val ggc = preGlow.createGraphics()
+		ggc.setPaint(new Color(0, 0, 0, 64))
+		ggc.fillRect(padding, padding, iconSize, iconSize)
+		ggc.setComposite(AlphaComposite.DstIn)
+		ggc.drawImage(icon, null, padding, padding)
+		ggc.dispose()
+
+		val glow = new com.jhlabs.image.GaussianFilter(width / 8.0f).filter(preGlow, null)
+
+		val white = new BufferedImage(iconSize, iconSize, BufferedImage.TYPE_INT_ARGB)
+		val wgc = white.createGraphics()
+		wgc.setPaint(Color.white)
+		wgc.fillRect(0, 0, iconSize, iconSize)
+		wgc.setComposite(AlphaComposite.DstIn)
+		wgc.drawImage(icon, null, 0, 0)
+		wgc.dispose()
+
+		val asset = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB)
+		val gc = asset.createGraphics()
+		gc.drawImage(glow, null, 0, 0)
+		gc.drawImage(white, null, padding, padding)
+		gc.dispose()
+
+		asset
 	}
 
 	// http://developer.android.com/guide/practices/ui_guidelines/icon_design_status_bar.html#icon1
@@ -179,7 +246,7 @@ object Main {
 		asset
 	}
 
-	def renderNotificationV11(doc: SVGOMDocument, width: Int, height: Int): BufferedImage = {//
+	def renderNotificationV11(doc: SVGOMDocument, width: Int, height: Int): BufferedImage = {
 		// Create image filled with white.
 		val asset = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB)
 		val background = Color.white
